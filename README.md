804628d27371f4930f29a9957d3ce8f98df72391
DPS0340 <jh001007@gmail.com>
2018-12-21 00:18:04 +0900

    only readme

--- README.md
+++ README.md
@@ -1,12 +1,1 @@
-# DPSBot
-DPSBot-한국어 다용도 디스코드 봇
-
-한국어 다용도 디스코드 봇입니다.
-
-사용자가 봇을 직접 돌리기 위해서는 약간의 DB 설정이 필요합니다.
-
-초대 링크:https://discordapp.com/api/oauth2/authorize?client_id=523785272693882880&permissions=0&scope=bot
-
-http://ttakkku.tk/
-
-Proudly Powered by Team ttakkku
+ 
--- pluginlist.py
+++
@@ -1,4 +0,0 @@
-lst = ["admin", "archive", "crypto", "etc", "exec", "gravatar",
-       "mod", "osu", "post", "tag", "whisperandlog", "pluginsettings", "info"]
-alllst = ["admin", "aloh", "archive", "crypto", "etc", "exec", "gravatar",
-       "mod", "osu", "post", "tag", "whisperandlog", "pluginsettings", "info"]
