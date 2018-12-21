a3686a8d501bed30ec4dcd5f25e7db51b3aaf9d9
DPS0340 <jh001007@gmail.com>
2018-12-21 23:12:34 +0900

    dd

--- pluginlist.py
+++ pluginlist.py
@@ -1,4 +1,4 @@
 lst = ["admin", "archive", "crypto", "etc", "exec", "gravatar",
-       "mod", "osu", "post", "tag", "whisperandlog", "pluginsettings", "info"]
+       "mod", "osu", "post", "tag", "whisperandlog", "pluginsettings", "info", "transconfig"]
 alllst = ["admin", "aloh", "archive", "crypto", "etc", "exec", "gravatar",
-       "mod", "osu", "post", "tag", "whisperandlog", "pluginsettings", "info"]
+       "mod", "osu", "post", "tag", "whisperandlog", "pluginsettings", "info", "transconfig"]
