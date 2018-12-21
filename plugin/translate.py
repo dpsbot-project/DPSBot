3820f0ba6388fc2de418611f7b447b6f3c570eb1
DPS0340 <jh001007@gmail.com>
2018-12-21 22:40:13 +0900

    translate module prototype

--- plugin/translate.py
+++ plugin/translate.py
@@ -1,1 +1,21 @@
- 
+import googletrans
+
+class Trans():
+    def __init__(self):
+        self.translator = googletrans.Translator()
+        self.outputlang = 'kr'
+    
+    def setlang(self, lang: str):
+        self.outputlang = lang
+
+    def gettext(self, text: str):
+        if self.outputlang == 'kr':
+            return text
+        return self.translator.translate(text, dest=self.outputlang).text
+    
+    def temprun(self, text: str, lang:str):
+        if lang == 'kr':
+            return text
+        return self.translator.translate(text, dest=lang).text   
+
+trans = Trans()
