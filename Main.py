e06207f716b6049444503fe9f2e18f8d39cc32fa
DPS0340 <jh001007@gmail.com>
2018-12-21 23:01:20 +0900

    hmm

--- Main.py
+++ Main.py
@@ -3,7 +3,23 @@ import discord
 import asyncio
 from variables import token, pluginfolder, gamename, prefix, owner
 from pluginlist import lst as initial_extensions
-bot = commands.Bot(command_prefix=prefix.get())
+from translate import trans
+class DPSBot(commands.Bot):
+    @asyncio.coroutine
+    def send_message(self, destination, content=None, *, tts=False, embed=None):
+        channel_id, guild_id = yield from self._resolve_destination(destination)
+
+        content = str(content) if content is not None else None
+        content = trans.gettext(content) if content is not None else None
+        
+        if embed is not None:
+            embed = embed.to_dict()
+
+        data = yield from self.http.send_message(channel_id, content, guild_id=guild_id, tts=tts, embed=embed)
+        channel = self.get_channel(data.get('channel_id'))
+        message = self.connection._create_message(channel=channel, **data)
+        return message
+bot = DPSBot(command_prefix=prefix.get())
 
 @bot.event
 async def on_ready():
