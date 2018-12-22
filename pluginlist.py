b4a18aeeb96bcff29350895bd4ed47e5921c4d9d
DPS0340 <jh001007@gmail.com>
2018-12-22 17:13:33 +0900

    removing archive in default enabled plugin. however i believe possibility. so not move to legacy

--- pluginlist.py
+++ pluginlist.py
@@ -1,4 +1,4 @@
-lst = ["admin", "archive", "crypto", "etc", "exec", "gravatar",
+lst = ["admin", "crypto", "etc", "exec", "gravatar",
        "mod", "osu", "post", "tag", "whisperandlog", "pluginsettings", "info", "transhandler"]
 alllst = ["admin", "aloh", "archive", "crypto", "etc", "exec", "gravatar",
        "mod", "osu", "post", "tag", "whisperandlog", "pluginsettings", "info", "transhandler"]
