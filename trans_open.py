04be5b3a679134494f16980a564c13a332d6052b
DPS0340 <jh001007@gmail.com>
2018-12-23 04:26:42 +0900

    dd

--- trans_open.py
+++ trans_open.py
@@ -3,10 +3,10 @@ import gettext
 
 class Opentrans():
     def __init__(self):
-        self.lang = gettext.translation('./', localedir='./locales', languages=[trans.get()], fallback=True)
+        self.lang = gettext.translation('data', localedir='./locales', languages=[trans.get()], fallback=True)
         self.lang.install()
     def refresh(self):
-        self.lang = gettext.translation('./', localedir='./locales', languages=[trans.get()], fallback=True)
+        self.lang = gettext.translation('data', localedir='./locales', languages=[trans.get()], fallback=True)
         self.lang.install()
 
 opentrans = Opentrans()
