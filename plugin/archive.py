2966451978b9e73e391b3073873e11a302e68d58
DPS0340 <jh001007@gmail.com>
2018-12-22 17:07:34 +0900

    geckodriver path test

--- plugin/archive.py
+++ plugin/archive.py
@@ -28,7 +28,7 @@ class archiveclass():
         self.profile = webdriver.FirefoxProfile()
         self.profile.set_preference("media.volume_scale", "0.0")
         self.profile.set_preference("intl.accept_languages", "ko")
-        self.driver = webdriver.Firefox(self.profile, firefox_options=self.options, executable_path="geckodriver")
+        self.driver = webdriver.Firefox(self.profile, firefox_options=self.options, executable_path="/app/geckodriver")
         self.options.add_argument(
                 {'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_3_2 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13F69 Safari/601.1', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
                 'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3', 'Accept-Encoding': 'none', 'Accept-Language': 'en-US,en;q=0.8', 'Connection': 'keep-alive'})
