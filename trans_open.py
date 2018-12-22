204f26e981d21143ea79a46f4a4b9ff5391a9e15
DPS0340 <jh001007@gmail.com>
2018-12-23 03:42:37 +0900

    dd

--- trans_open.py
+++ trans_open.py
@@ -1,6 +1,6 @@
 from translate import trans
 import gettext
-_ = gettext.translation('data', localedir='locales', languages=[trans.outputlang])
+_ = gettext.translation('data', localedir='locales', languages=[trans.outputlang], fallback=True)
 def refresh():
     global _
-    _ = gettext.translation('data', localedir='locales', languages=[trans.outputlang])
+    _ = gettext.translation('data', localedir='locales', languages=[trans.outputlang], fallback=True)
