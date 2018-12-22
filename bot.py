2077ac1583cbb0363baada6630b235ec00b858bb
DPS0340 <jh001007@gmail.com>
2018-12-22 14:11:16 +0900

    make it simple

--- bot.py
+++ bot.py
@@ -3,24 +3,14 @@ from discord.ext import commands
 import asyncio
 
 class DPSBot(commands.Bot):
+    def __init__(self):
+        self.raw_send_message = self.send_message
+
     @asyncio.coroutine
     def send_message(self, destination, content=None, *, tts=False, embed=None):
         channel_id, guild_id = yield from self._resolve_destination(destination)
 
         content = trans.gettext(str(content)) if content is not None else None
-        
-        if embed is not None:
-            embed = embed.to_dict()
-
-        data = yield from self.http.send_message(channel_id, content, guild_id=guild_id, tts=tts, embed=embed)
-        channel = self.get_channel(data.get('channel_id'))
-        message = self.connection._create_message(channel=channel, **data)
-        return message
-    @asyncio.coroutine
-    def raw_send_message(self, destination, content=None, *, tts=False, embed=None):
-        channel_id, guild_id = yield from self._resolve_destination(destination)
-
-        content = str(content) if content is not None else None
         
         if embed is not None:
             embed = embed.to_dict()
