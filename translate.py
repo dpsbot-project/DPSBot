789723f8a6f4b2405a69dda273b225e96c0e30a4
DPS0340 <jh001007@gmail.com>
2018-12-23 03:35:30 +0900

    dd

--- translate.py
+++ translate.py
@@ -3,14 +3,12 @@ import googletrans
 class Trans():
     def __init__(self):
         self.translator = googletrans.Translator()
-        self.outputlang = 'kr'
+        self.outputlang = 'ko-KR'
     
     def setlang(self, lang: str):
         self.outputlang = lang
 
     def gettext(self, text: str):
-        if self.outputlang == 'kr':
-            return text
         return self.translator.translate(text, dest=self.outputlang).text
     
     def temprun(self, text: str, src:str, dest:str):
