78061daca8641c22769df171c48c2c446bc86847
DPS0340 <jh001007@gmail.com>
2018-12-24 15:22:24 +0900

    hmm......

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ def server_load(id):
     with open('servers/%s.json' % id, 'r') as r:
         return json.load(r)
 def server_init(bot):
-    for server in bot.servers:
+    for server in list(bot.servers):
         id = server.id
         if os.path.exists('servers/%s.json' % id):
             pass
