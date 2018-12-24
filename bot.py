16f75036669eeb999565331791da1a4786561947
jiho lee <jh001007@gmail.com>
2018-12-24 12:37:36 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,18 @@ import asyncio
 
 class DPSBot(commands.Bot):
     @asyncio.coroutine
+    def send_message(self, destination, content=None, *, tts=False, embed=None):
+        channel_id, guild_id = yield from self._resolve_destination(destination)
+        content = trans.gettext(str(content)) if content is not None else None
+        
+        if embed is not None:
+            embed = embed.to_dict()
+
+        data = yield from self.http.send_message(channel_id, content, guild_id=guild_id, tts=tts, embed=embed)
+        channel = self.get_channel(data.get('channel_id'))
+        message = self.connection._create_message(channel=channel, **data)
+        return message
+    @asyncio.coroutine
     def raw_send_message(self, destination, content=None, *, tts=False, embed=None):
         channel_id, guild_id = yield from self._resolve_destination(destination)
 
