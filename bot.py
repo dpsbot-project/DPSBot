1dc018aa041309f60ca21461a15ae21ccac586f3
DPS0340 <jh001007@gmail.com>
2018-12-24 17:15:55 +0900

    hmm

--- bot.py
+++ bot.py
@@ -7,15 +7,13 @@ class DPSBot(commands.Bot):
     @asyncio.coroutine
     def send_message(self, destination, content=None, *, tts=False, embed=None):
         channel_id, guild_id = yield from self._resolve_destination(destination)
-        content = trans.gettext(str(content)) if content is not None else None
+        content = trans.gettext_remote(str(content), serverlist.list[int(guild_id)]['language']) if content is not None else None
         
         if embed is not None:
             embed = embed.to_dict()
 
         data = yield from self.http.send_message(channel_id, content, guild_id=guild_id, tts=tts, embed=embed)
         channel = self.get_channel(data.get('channel_id'))
-        serverid = data.get('server_id')
-        trans.gettext_remote(data, serverlist.list[int(serverid)]['language'])
         message = self.connection._create_message(channel=channel, **data)
         return message
     @asyncio.coroutine
