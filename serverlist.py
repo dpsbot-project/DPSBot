ff69a01c89294bc69ae9f82288526cba784fef01
DPS0340 <jh001007@gmail.com>
2018-12-24 16:15:42 +0900

    dd

--- serverlist.py
+++ serverlist.py
@@ -1,3 +1,5 @@
-from Main import bot
-from server import Serverlist
-serverlist = Serverlist(bot) 
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from bot import DPSBot
+    from server import Serverlist
+serverlist = Serverlist(DPSBot())
