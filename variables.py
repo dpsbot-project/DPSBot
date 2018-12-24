7b875556c77804646c618b54ce5110ab002b3ab6
DPS0340 <jh001007@gmail.com>
2018-12-24 13:25:49 +0900

    hmm

--- variables.py
+++ variables.py
@@ -1,6 +1,7 @@
 import os
 import psycopg2
 import trans_open
+from translate import trans
 DATABASE_URL = os.environ['DATABASE_URL']
 doinglist = [_('주인님 드릴 드립 커피를 내리고 있어요!'), _('두둥! 밴드부 활동 중이랍니다!'), _('요리 중이에요☆'), _('놀이터에서 꼬마들이랑 노는 중이랍니다!\n동심이란...후훗'),
              _('주인님의 블로그에 들일 가구들을 고르고 있어요!'), _('공부 중이랍니다!'), _('PUBG 플레이 중! 오늘은 진짜 치킨이에요!'), _('도서관에 왔어요! 현실속의 아카이브 저장소랍니다!')]
@@ -56,14 +57,14 @@ instructions = Instructions(instructionsvalue)
 gamename = Game()
 def gamerefresh():
     gamename.reset()
-    gamename.append(_("DPSBot!"))
+    gamename.append(trans.gettext(_("DPSBot!")))
     gamename.append(_("%shelp를 쳐보세요!") % prefixvalue)
     gamename.append(_("%s정보를 쳐보세요!") % prefixvalue)
-    gamename.append(_("제작자: DPS0340"))
-    gamename.append(_("dpsbot.tk"))
-    gamename.append(_("github.com/DPS0340/DPSbot"))
-    gamename.append(_("teamttakkku.tk"))
-    gamename.append(_("Proudly Powered by Team ttakkku"))
+    gamename.append(trans.gettext(_("제작자: DPS0340")))
+    gamename.append(trans.gettext(_("dpsbot.tk")))
+    gamename.append(trans.gettext(_("github.com/DPS0340/DPSbot")))
+    gamename.append(trans.gettext(_("teamttakkku.tk")))
+    gamename.append(trans.gettext(_("Proudly Powered by Team ttakkku")))
 
 gamerefresh()
 prefix = Prefix(prefixvalue)
