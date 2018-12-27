0e16b90c5fbd39f4378afc00d7b7f384e6c3203b
DPS0340 <jh001007@gmail.com>
2018-12-27 19:37:23 +0900

    dd

--- variables.py
+++ variables.py
@@ -1,6 +1,7 @@
 import os
 import psycopg2
 import trans_open
+import pickle
 DATABASE_URL = os.environ['DPSBOT_URL']
 doinglist = [_('주인님 드릴 드립 커피를 내리고 있어요!'), _('두둥! 밴드부 활동 중이랍니다!'), _('요리 중이에요☆'), _('놀이터에서 꼬마들이랑 노는 중이랍니다!\n동심이란...후훗'),
              _('주인님의 블로그에 들일 가구들을 고르고 있어요!'), _('공부 중이랍니다!'), _('PUBG 플레이 중! 오늘은 진짜 치킨이에요!'), _('도서관에 왔어요! 현실속의 아카이브 저장소랍니다!')]
@@ -32,16 +33,20 @@ class Instructions():
         self.value = value
 class Game():
     def __init__(self):
-        self.list = []
+        with open("./gamelist", "rb") as r:
+            self.list = pickle.load(r)
 
     def get(self):
         return self.list
 
     def append(self, splash):
         self.list.append(splash)
+        with open("./gamelist", "wb") as w:
+            pickle.dump(self.list, w)
 
     def reset(self):
-        self.list = []
+        with open("./gamelist", "rb") as r:
+            self.list = pickle.load(r)
 class Prefix():
     def __init__(self, value):
         self.value = value
@@ -51,18 +56,9 @@ class Prefix():
 
     def set(self, value):
         self.value = value
+games = ["DPSBot!","type %shelp!" % prefixvalue,"type %sinfo!" % prefixvalue,"Made by DPS0340","dpsbot.tk","github.com/DPS0340/DPSbot","ttakkku.tk","Proudly Powered by Team ttakkku"]
+with open("./gamelist", "wb") as w:
+    pickle.dump(games, w)
 instructions = Instructions(instructionsvalue)
 gamename = Game()
-def gamerefresh():
-    gamename.reset()
-    gamename.append("DPSBot!")
-    gamename.append("type %shelp!" % prefixvalue)
-    gamename.append("type %sinfo!" % prefixvalue)
-    gamename.append("Made by DPS0340")
-    gamename.append("dpsbot.tk")
-    gamename.append("github.com/DPS0340/DPSbot")
-    gamename.append("teamttakkku.tk")
-    gamename.append("Proudly Powered by Team ttakkku")
-
-gamerefresh()
 prefix = Prefix(prefixvalue)
