870ef573d66260aad59a07227d6288fa5acf8b36
jiho lee <jh001007@gmail.com>
2019-02-03 04:36:37 +0900

    Update admin.py

--- plugin/default/admin.py
+++ plugin/default/admin.py
@@ -154,12 +154,14 @@ class adminclass():
             await self.bot.say(_('권한이 없습니다.\n봇 개발자만 사용 가능합니다.'))
 
     @commands.command(hidden=False, pass_context=True)
-    @commands.has_permissions(ban_members=True)
     async def ban(self, ctx, member: discord.Member, days: int = 1, *, reason="None"):
         try:
-            await self.bot.ban(member, days)
-            await self.bot.say(_('%s 님이 %s님을 %s일간 밴하셨습니다.') % (ctx.message.author.name, member.name, days))
-            await self.bot.say(_('이유:%s') % reason)
+            if ctx.message.author == owner:
+                await self.bot.ban(member, days)
+                await self.bot.say(_('%s 님이 %s님을 %s일간 밴하셨습니다.') % (ctx.message.author.name, member.name, days))
+                await self.bot.say(_('이유:%s') % reason)
+            else:
+                pass
         except:
             await self.bot.say(_('밴 대상자가 없거나 봇에 밴 권한이 없습니다.'))
 
