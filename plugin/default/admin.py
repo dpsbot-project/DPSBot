d18a33d409a40074eec6a303cbd8c91a8179787d
jiho lee <jh001007@gmail.com>
2019-02-20 01:00:33 +0900

    Update admin.py

--- plugin/default/admin.py
+++ plugin/default/admin.py
@@ -160,9 +160,6 @@ class adminclass():
                 await self.bot.ban(member, days)
                 await self.bot.say(_('%s 님이 %s님을 %s일간 밴하셨습니다.') % (ctx.message.author.name, member.name, days))
                 await self.bot.say(_('이유:%s') % reason)
-            else:
-                await self.bot.say("권한이 없습니다!")
-                pass
         except:
             await self.bot.say(_('밴 대상자가 없거나 봇에 밴 권한이 없습니다.'))
 
